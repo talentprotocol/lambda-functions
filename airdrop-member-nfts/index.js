0651f4b9c6996b393cb946400575cd07fdafed25
francisco-leal <francisco@talentprotocol.com>
2022-03-24 16:11:22 +0000

    Add image upload and replace token uri

--- airdrop-member-nfts/index.js
+++ airdrop-member-nfts/index.js
@@ -2,12 +2,33 @@ const { ethers } = require("ethers");
 const { NFTStorage, File } = require("nft.storage");
 const { CeloProvider, CeloWallet } = require('@celo-tools/celo-ethers-wrapper');
 const fs = require("fs");
+const Axios = require('axios');
+const mime = require("mime");
 
 const WALLET_PK = process.env.WALLET_PK;
 const NFT_ADDRESS = process.env.NFT_ADDRESS;
 const PROVIDER_URL = process.env.PROVIDER_URL;
 const TOKEN = process.env.NFT_STORAGE_TOKEN;
 const SEASON = process.env.SEASON;
+
+async function downloadImage(url, filepath) {
+  const response = await Axios({
+      url,
+      method: 'GET',
+      responseType: 'stream'
+  });
+  return new Promise((resolve, reject) => {
+      response.data.pipe(fs.createWriteStream(filepath))
+          .on('error', reject)
+          .once('close', () => resolve(filepath)); 
+  });
+}
+
+async function fileFromPath(filePath) {
+  const content = await fs.promises.readFile(filePath)
+  const type = mime.getType(filePath)
+  return new File([content], filePath.substring(5, filePath.length), { type })
+}
 
 exports.handler = async (event) => {
   try{
@@ -26,7 +47,7 @@ exports.handler = async (event) => {
       const balance = await contract.balanceOf(event.wallet_id);
 
       if (balance > 0) {
-        const tokenId = contract.tokenOfOwnerByIndex(event.wallet_id, 0);
+        const tokenId = await contract.tokenOfOwnerByIndex(event.wallet_id, 0);
         return {
           statusCode: 400,
           body: {
@@ -48,20 +69,19 @@ exports.handler = async (event) => {
 
       if (event.image_url) {
         const client = new NFTStorage({ token: TOKEN });
-        let imgData;
 
-        const url = await fetch(event.image_url);
-        imgData = await url.blob();
+        await downloadImage(event.image_url, `/tmp/${event.image_name}`);
+        const image = await fileFromPath(`/tmp/${event.image_name}`)
 
         const metadata = await client.store({
           name: 'Talent Protocol Community Member NFT',
           description: 'Talent Protocol Community Member level NFT. Owners of this NFT are considered members of Talent Protocol',
-          image: imgData && new File([imgData], event.image_name, { type: "image/jpg" }),
+          image,
           properties: {
             type: "image",
             season: SEASON,
           }
-        });
+        })
   
         await contract.connect(owner).setTokenURI(tokenId, metadata.url);
       }
