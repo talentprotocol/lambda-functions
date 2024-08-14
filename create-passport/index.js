67744f55108b81f5e7db3635bb01735051a18894
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2024-08-14 15:02:15 +0100

    Update create passport function to handle multiple passports

--- create-passport/index.js
+++ create-passport/index.js
@@ -1,13 +1,12 @@
 import passportContract from "./PassportABI.json" with { type: "json" };
 
-import { createPublicClient, http, getContract } from "viem";
+import { createPublicClient, http, encodeFunctionData } from "viem";
 import { baseSepolia, base } from "viem/chains";
 import { createSmartAccountClient } from "permissionless";
 import { privateKeyToSimpleSmartAccount } from "permissionless/accounts";
 import { createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
 
 const ENTRYPOINT = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
-const WALLET_PK = process.env.WALLET_PK;
 
 export const handler = async (event) => {
   try {
@@ -15,14 +14,17 @@ export const handler = async (event) => {
     const paymasterUrl = event.paymaster_url;
     const bundlerUrl = event.bundler_url;
     const passportRegistryAddress = event.passport_registry_address;
-    const source = event.source;
     const environment = event.environment;
-    const passportId = event.passport_id;
+    const passports = event.passports;
     let chain = baseSepolia;
-    const primaryWallet = event.primary_wallet;
+    let walletPK = process.env.SEPOLIA_WALLET_PK;
+
+    console.log("Uploading: ", passports)
 
     if(environment == "mainnet") {
+      console.log("Mainnet")
       chain = base;
+      walletPK = process.env.WALLET_PK
     } 
 
     const publicClient = createPublicClient({
@@ -32,7 +34,7 @@ export const handler = async (event) => {
     console.log("Initialize adminSmartAccount");
 
     const adminSmartAccount = await privateKeyToSimpleSmartAccount(publicClient, {
-			privateKey: WALLET_PK,
+			privateKey: walletPK,
 			entryPoint: ENTRYPOINT, // global entrypoint
 			factoryAddress: "0x9406Cc6185a346906296840746125a0E44976454",
 		});
@@ -58,38 +60,37 @@ export const handler = async (event) => {
       },
     });
 
-    console.log("Initialize Passport registry");
+    console.log("adminSmartAccount Address", adminSmartAccount.address);
 
-    const passportRegistry = getContract({
-      address: passportRegistryAddress,
-      abi: passportContract.abi,
-      client: {
-        public: publicClient,
-        wallet: adminSmartAccountClient,
-      },
-    });
 
-    console.log(`Call admin create for wallet: ${primaryWallet}`);
+    const sponsoredTransactions = passports.map((passport) => { 
+      return {
+        to: passportRegistryAddress,
+        data: encodeFunctionData({abi: passportContract.abi, functionName: "adminCreate", args: [passport.source, passport.wallet, passport.id]}),
+        value: BigInt(0)
+      }
+    })
 
-    const txHash = await passportRegistry.write.adminCreate([
-      source,
-      primaryWallet,
-      passportId,
-    ]);
+    console.log("Send batch transactions");
+
+    const txHash = await adminSmartAccountClient.sendTransactions({
+      transactions: sponsoredTransactions
+    })
 
     console.log(
       `UserOperation included: https://sepolia.basescan.org/tx/${txHash}`,
     );
 
     const transaction = await publicClient.waitForTransactionReceipt({
-      confirmations: 5,
       hash: txHash,
     });
+
+    console.log("transaction:", transaction);
 
     const responseBody = {
       statusCode: 200,
       body: {
-        tx_hash: transaction.hash
+        tx_hash: transaction.transactionHash
       }
     };
 
